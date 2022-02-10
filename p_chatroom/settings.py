d9dbef0f21ede3183642c826cf151405785994ea
shakthi876 <97612729+shakthi876@users.noreply.github.com>
2022-02-10 09:43:30 +0530

    Update settings.py

--- p_chatroom/settings.py
+++ p_chatroom/settings.py
@@ -124,7 +124,7 @@ import os
 import django_heroku
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
-django_heroku.setting(locals())
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
